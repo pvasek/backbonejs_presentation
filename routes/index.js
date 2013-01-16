a12a141e0d63f34a5680c2723895bf8c3632dab0
pvasek <pvasek@gmail.com>
2013-01-16 21:51:19 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var fs = require('fs')
+    , hbs = require('hbs');
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+    var pagesDir = req.app.get('pages directory');
+
+    // reload handlebars partials
+    fs.readdirSync(pagesDir).forEach(function(file) {
+        var pageName = file.replace('.html', '');
+        hbs.registerPartial(pageName, fs.readFileSync(pagesDir + file, 'utf8'));
+    });
+
+  res.render('index', { title: 'Express' });
+};
